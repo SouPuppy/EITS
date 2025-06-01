03226fa319b5c06f0ce282b502583f7be3c37fbe
soupup <wdk1745@gmail.com>
2025-06-02 02:15:09 +0800

    fix(typo)

--- include/Machinish/Machinish.h
+++ include/Machinish/Machinish.h
@@ -4,7 +4,7 @@
 
 #include <Machinish/info/version.h>
 #include <Machinish/kernel/type.h>
-#include <Machinish/kernel/contex.h>
+#include <Machinish/kernel/context.h>
 
 #include <Machinish/kernel/Forms/pi.h>
 #include <Machinish/kernel/Forms/typed_variable.h>
