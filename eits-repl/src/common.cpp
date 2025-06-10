c0aa9da6f6ca515c565106ca4a43ad1393ec1e29
soupup <wdk1745@gmail.com>
2025-06-11 00:07:01 +0800

    [repl]: update dictioanry

--- eits-repl/src/common.cpp
+++ eits-repl/src/common.cpp
@@ -1,11 +1,47 @@
 #include "common.h"
 
 std::unordered_map<std::string, std::string> replacement_table = {
-	{"pi", "Π"},
-	{"sigma", "Σ"}, 
-	{"lambda", "λ"},
-	{"->", "→"}, 
-	{"=>", "⇒"}, 
-	{"forall", "∀"}, 
-	{"exists", "∃"},
+	// Greek
+	{" alpha", " α"},
+	{" beta", " β"},
+	{" gamma", " γ"},
+	{" lambda", " λ"},
+
+	// Capital Greek
+	{" Gamma", " Γ"},
+	{" Pi", " Π"},
+	{" Sigma", " Σ"},
+
+	// Math
+	{" ->", " →"},
+	{" =>", " ⇒"},
+	{" times", " ×"},
+
+	// Logic
+	{" neg", " ¬"},
+	{" false", " ⊥"},
+	{" true", " ⊤"},
+
+	// Set
+	{" emptyset", " ∅"},
+	{" in", " ∈"},
+	{" notin", " ∉"},
+	{" subset", " ⊂"},
+	{" subseteq", " ⊆"},
+	{" and", " ∩"},
+	{" or", " ∪"},
+
+	// Type Theory
+	{" |-", " ⊢"}, {" vdash", " ⊢"},
+	{" |=", " ⊨"}, {" models", " ⊨"},
+	{" circ", " ∘"},
+
+	// Special Math
+	{" bbN", " ℕ"},
+	{" bbZ", " ℤ"},
+	{" bbQ", " ℚ"},
+	{" bbR", " ℝ"},
+	{" bbC", " ℂ"},
+	{" bbP", " ℙ"},
+	{" bbE", " 𝔼"},
 };
